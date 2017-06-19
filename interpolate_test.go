39227d3a3d7658060e3e509c55fc244bfebf2878
Masashi Fujita <objectxtreme@gmail.com>
2017-06-20 03:34:07 +0900

    chore: Use `GoConvey` as a test framework

--- interpolate_test.go
+++ interpolate_test.go
@@ -3,101 +3,152 @@ package main
 import (
 	"testing"
 
-	"github.com/stretchr/testify/assert"
-	"github.com/stretchr/testify/suite"
+	. "github.com/smartystreets/goconvey/convey"
 )
 
-type InterpolateTestSuite struct {
-	suite.Suite
-	VariableDictionary map[string]string
+func TestInterpolateLiteral(t *testing.T) {
+	dict := newDictionary()
+
+	Convey("Test with literals", t, func() {
+		Convey("\"\" should be \"\"", func() {
+			actual, err := Interpolate("", dict)
+			So(err, ShouldBeNil)
+			So(actual, ShouldEqual, "")
+		})
+		Convey("\"literal\" should be \"literal\"", func() {
+			actual, err := Interpolate("literal", dict)
+			So(err, ShouldBeNil)
+			So(actual, ShouldEqual, "literal")
+		})
+		Convey("\"foobarbaz$\" should be \"foobarbaz$\"", func() {
+			actual, err := Interpolate("foobarbaz$", dict)
+			So(err, ShouldBeNil)
+			So(actual, ShouldEqual, "foobarbaz$")
+		})
+	})
 }
 
-func (suite *InterpolateTestSuite) SetupTest() {
-	suite.VariableDictionary = map[string]string{
+func TestInterpolateExpansion(t *testing.T) {
+	dict := newDictionary()
+
+	Convey("Test expansions", t, func() {
+		Convey("Single level expansion", func() {
+			actual, err := Interpolate("${foo}", dict)
+			So(err, ShouldBeNil)
+			So(actual, ShouldEqual, "foo-value")
+		})
+		Convey("Nested expansion", func() {
+			actual, err := Interpolate("${baz}", dict)
+			So(err, ShouldBeNil)
+			So(actual, ShouldEqual, "baz-value, bar-value, foo-value")
+		})
+		Convey("Double $$ in string", func() {
+			actual, err := Interpolate("${foo}$$${bar}", dict)
+			So(err, ShouldBeNil)
+			So(actual, ShouldEqual, "foo-value$bar-value, foo-value")
+		})
+		Convey("Non existants", func() {
+			actual, err := Interpolate("${mokeke}moke", dict)
+			So(err, ShouldBeNil)
+			So(actual, ShouldEqual, "moke")
+		})
+	})
+}
+
+func TestInterpolateError(t *testing.T) {
+	dict := newDictionary()
+
+	Convey("Errors", t, func() {
+		Convey("Unmatched {}", func() {
+			_, err := Interpolate("${foo", dict)
+			So(err, ShouldNotBeNil)
+			So(err.Error(), ShouldContainSubstring, "{foo")
+		})
+		Convey("Passthrough unrecognized", func() {
+			actual, err := Interpolate("$foo", dict)
+			So(err, ShouldBeNil)
+			So(actual, ShouldEqual, "$foo")
+		})
+		Convey("Exceeding recursion limit", func() {
+			_, err := Interpolate("${rec}", dict)
+			So(err, ShouldNotBeNil)
+			So(err.Error(), ShouldEqual, "Recursion limit exceeded.")
+		})
+	})
+}
+
+func TestStrictInterpolateLiteral(t *testing.T) {
+	dict := newDictionary()
+
+	Convey("Test with literals", t, func() {
+		Convey("\"\" should be \"\"", func() {
+			actual, err := StrictInterpolate("", dict)
+			So(err, ShouldBeNil)
+			So(actual, ShouldEqual, "")
+		})
+		Convey("\"literal\" should be \"literal\"", func() {
+			actual, err := StrictInterpolate("literal", dict)
+			So(err, ShouldBeNil)
+			So(actual, ShouldEqual, "literal")
+		})
+		Convey("\"foobarbaz$\" should be \"foobarbaz$\"", func() {
+			actual, err := StrictInterpolate("foobarbaz$", dict)
+			So(err, ShouldBeNil)
+			So(actual, ShouldEqual, "foobarbaz$")
+		})
+	})
+}
+
+func TestStrictInterpolateExpansion(t *testing.T) {
+	dict := newDictionary()
+	Convey("Test expansions", t, func() {
+		Convey("Single level expansion", func() {
+			actual, err := StrictInterpolate("${foo}", dict)
+			So(err, ShouldBeNil)
+			So(actual, ShouldEqual, "foo-value")
+		})
+		Convey("Nested expansion", func() {
+			actual, err := StrictInterpolate("${baz}", dict)
+			So(err, ShouldBeNil)
+			So(actual, ShouldEqual, "baz-value, bar-value, foo-value")
+		})
+		Convey("Double $$ in string", func() {
+			actual, err := StrictInterpolate("${foo}$$${bar}", dict)
+			So(err, ShouldBeNil)
+			So(actual, ShouldEqual, "foo-value$bar-value, foo-value")
+		})
+	})
+}
+func TestStrictInterpolateErrors(t *testing.T) {
+	dict := newDictionary()
+	Convey("Errors", t, func() {
+		Convey("Unmatched {}", func() {
+			_, err := StrictInterpolate("${foo", dict)
+			So(err, ShouldNotBeNil)
+			So(err.Error(), ShouldContainSubstring, "{foo")
+		})
+		Convey("Unrecognized as error", func() {
+			_, err := StrictInterpolate("$foo", dict)
+			So(err, ShouldNotBeNil)
+			So(err.Error(), ShouldEqual, "Invalid `$` sequence \"foo\" found.")
+		})
+		Convey("Non existants", func() {
+			_, err := StrictInterpolate("${mokeke}moke", dict)
+			So(err, ShouldNotBeNil)
+			So(err.Error(), ShouldEqual, "Unknown reference ${mokeke} found.")
+		})
+		Convey("Exceeding recursion limit", func() {
+			_, err := StrictInterpolate("${rec}", dict)
+			So(err, ShouldNotBeNil)
+			So(err.Error(), ShouldEqual, "Recursion limit exceeded.")
+		})
+	})
+}
+
+func newDictionary() map[string]string {
+	return map[string]string{
 		"foo": "foo-value",
 		"bar": "bar-value, ${foo}",
 		"baz": "baz-value, ${bar}",
 		"rec": "do${rec}"}
-}
-
-// Interpolate empty string
-func (suite *InterpolateTestSuite) TestEmpty() {
-	actual, err := Interpolate("", suite.VariableDictionary)
-	if assert.NoError(suite.T(), err) {
-		assert.Equal(suite.T(), "", actual)
-	}
-}
-
-// Interpolate literal
-func (suite *InterpolateTestSuite) TestLiteral() {
-	actual, err := Interpolate("literal", suite.VariableDictionary)
-	if assert.NoError(suite.T(), err) {
-		assert.Equal(suite.T(), "literal", actual)
-	}
-}
-
-func (suite *InterpolateTestSuite) TestDollerTerminated() {
-	actual, err := Interpolate("foobarbaz$", suite.VariableDictionary)
-	if assert.NoError(suite.T(), err) {
-		assert.Equal(suite.T(), "foobarbaz$", actual)
-	}
-}
-
-func (suite *InterpolateTestSuite) TestSimpleSubstitution() {
-	actual, err := Interpolate("${foo}", suite.VariableDictionary)
-	if assert.NoError(suite.T(), err) {
-		assert.Equal(suite.T(), "foo-value", actual)
-	}
-}
-
-func (suite *InterpolateTestSuite) TestNestedSubstitution() {
-	actual, err := Interpolate("${baz}", suite.VariableDictionary)
-	if assert.NoError(suite.T(), err) {
-		assert.Equal(suite.T(), "baz-value, bar-value, foo-value", actual)
-	}
-}
-
-func (suite *InterpolateTestSuite) TestDoubleDoller() {
-	actual, err := Interpolate("${foo}$$${bar}", suite.VariableDictionary)
-	if assert.NoError(suite.T(), err) {
-		assert.Equal(suite.T(), "foo-value$bar-value, foo-value", actual)
-	}
-}
-
-func (suite *InterpolateTestSuite) TestUnmatchedError() {
-	_, err := Interpolate("${foo", suite.VariableDictionary)
-	assert.EqualError(suite.T(), err, "Unmatched `{` found after \"{foo\".")
-}
-
-func (suite *InterpolateTestSuite) TestInvalidError() {
-	actual, err := Interpolate("$foo", suite.VariableDictionary)
-	if assert.NoError(suite.T (), err) {
-		assert.Equal (suite.T (), actual, "$foo")
-	}
-}
-
-func (suite *InterpolateTestSuite) TestInvalidErrorStrict() {
-	_, err := StrictInterpolate("$foo", suite.VariableDictionary)
-	assert.EqualError(suite.T (), err, "Invalid `$` sequence \"foo\" found.")
-}
-
-func (suite *InterpolateTestSuite) TestRecursion() {
-	_, err := Interpolate("${rec}", suite.VariableDictionary)
-	assert.EqualError(suite.T(), err, "Recursion limit exceeded.")
-}
-
-func (suite *InterpolateTestSuite) TestNonExistent() {
-	actual, err := Interpolate("${mokeke}moke", suite.VariableDictionary)
-	if assert.NoError(suite.T(), err) {
-		assert.Equal(suite.T(), actual, "moke")
-	}
-}
-
-func (suite *InterpolateTestSuite) TestNonExistentWithStrict () {
-	_, err := StrictInterpolate("${mokeke}moke", suite.VariableDictionary)
-	assert.EqualError(suite.T(), err, "Unknown reference ${mokeke} found.")
-}
-
-func TestInterpolateSuite(t *testing.T) {
-	suite.Run(t, new(InterpolateTestSuite))
 }
