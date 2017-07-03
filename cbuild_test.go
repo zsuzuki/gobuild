016610198e147c7c106bba5f8257518ecde50696
Masashi Fujita <objectxtreme@gmail.com>
2017-07-03 22:25:20 +0900

    test: Tidy-up

--- cbuild_test.go
+++ cbuild_test.go
@@ -8,88 +8,102 @@ import (
 )
 
 func TestToBooleanTrue(t *testing.T) {
-	Convey("Test `ToBoolean` return true case", t, func() {
+	Convey("GIVEN: Test cases", t, func() {
 		trueCases := []string{
 			"true", "True", "TRUE", "T",
 			"yes", "Yes", "YES", "Y",
 			"on", "On", "ON",
 			"1"}
 		for _, v := range trueCases {
-			Convey(fmt.Sprintf("\"%s\" should be `true`", v), func() {
+			Convey(fmt.Sprintf("WHEN: Evaluating \"%s\"", v), func() {
 				actual := ToBoolean(v)
-				So(actual, ShouldBeTrue)
-			})
-			addJunk := v + " ABCDEFG"
-			Convey(fmt.Sprintf("\"%s\" should be `true`", addJunk), func() {
-				actual := ToBoolean(v)
-				So(actual, ShouldBeTrue)
+				Convey("THEN: Should be `true`", func() {
+					So(actual, ShouldBeTrue)
+				})
 			})
 		}
-
+		for _, v := range trueCases {
+			addJunk := v + " ABCDEFG"
+			Convey(fmt.Sprintf("WHEN: Evaluationg \"%s\"", addJunk), func() {
+				actual := ToBoolean(v)
+				Convey("THEN: Should be `true`", func() {
+					So(actual, ShouldBeTrue)
+				})
+			})
+		}
 	})
 }
 
 func TestToBooleanFalse(t *testing.T) {
-	Convey("Test `ToBoolean` returns `false` case", t, func() {
+	Convey("GIVEN: Test cases", t, func() {
 		falseCases := []string{
 			"false", "False", "FALSE", "F",
 			"no", "No", "NO", "N",
 			"off", "Off", "OFF",
 			"0"}
 		for _, v := range falseCases {
-			Convey(fmt.Sprintf("\"%s\" should be `false`", v), func() {
+			Convey(fmt.Sprintf("WHEN: Evaluating \"%s\"", v), func() {
 				actual := ToBoolean(v)
-				So(actual, ShouldBeFalse)
+				Convey("THEN: Should be `false`", func() {
+					So(actual, ShouldBeFalse)
+				})
 			})
+		}
+		for _, v := range falseCases {
 			addJunk := v + " ABCDEFG"
-			Convey(fmt.Sprintf("\"%s\" should be `false`", addJunk), func() {
+			Convey(fmt.Sprintf("WHEN: Evaluating \"%s\"", addJunk), func() {
 				actual := ToBoolean(v)
-				So(actual, ShouldBeFalse)
+				Convey("THEN: Should be `false`", func() {
+					So(actual, ShouldBeFalse)
+				})
 			})
 		}
 	})
 }
 
 func TestToBooleanUndefined(t *testing.T) {
-	Convey("Test `ToBoolean` return `false` to unidentified string", t, func() {
+	Convey("GIVEN: Test cases", t, func() {
 		unknownCases := []string{
 			"mokeke", "truthy", "offset", "2"}
 		for _, v := range unknownCases {
-			Convey(fmt.Sprintf("\"%s\" should be `false`", v), func() {
+			Convey(fmt.Sprintf("WHEN: Evaluating \"%s\"", v), func() {
 				actual := ToBoolean(v)
-				So(actual, ShouldBeFalse)
+				Convey("THEN: Should be `false`", func() {
+					So(actual, ShouldBeFalse)
+				})
 			})
+		}
+		for _, v := range unknownCases {
 			addJunk := v + " ABCDEFG"
-			Convey(fmt.Sprintf("\"%s\" should be `false`", addJunk), func() {
+			Convey(fmt.Sprintf("WHEN: Evaluating \"%s\"", addJunk), func() {
 				actual := ToBoolean(v)
-				So(actual, ShouldBeFalse)
+				Convey("THEN: Should be `false`", func() {
+					So(actual, ShouldBeFalse)
+				})
 			})
 		}
 	})
 }
 
 func TestFixupCommandPath(t *testing.T) {
-	Convey("Test `FixupCommandPath`", t, func() {
+	Convey("GIVEN: Test cases", t, func() {
 		cases := []struct {
 			arg1st    string
 			arg2nd    string
 			expect1st string
 			expect2nd string
 		}{
-			{"abc def ghi", "/usr/local",
-				"/usr/local/abc def ghi", "/usr/local/abc"},
-			{"$abc def ghi", "/usr/local",
-				"/usr/local/$abc def ghi", "/usr/local/$abc"},
+			{"abc def ghi", "/usr/local", "/usr/local/abc def ghi", "/usr/local/abc"},
+			{"$abc def ghi", "/usr/local", "/usr/local/$abc def ghi", "/usr/local/$abc"},
 		}
 
 		for _, c := range cases {
-			Convey(fmt.Sprintf(`Test FixupCommandPath("%s", "%s") == ("%s", "%s")`,
-				c.arg1st, c.arg2nd,
-				c.expect1st, c.expect2nd), func() {
-
+			Convey(fmt.Sprintf(`WHEN: Evaluating FixupCommandPath("%s", "%s")`, c.arg1st, c.arg2nd), func() {
 				ea, eb := FixupCommandPath(c.arg1st, c.arg2nd)
-				So(ea, ShouldEqual, c.expect1st)
-				So(eb, ShouldEqual, c.expect2nd)
+				Convey(fmt.Sprintf("THEN: Should be (\"%s\", \"%s\")", c.expect1st, c.expect2nd), func() {
+					So(ea, ShouldEqual, c.expect1st)
+					So(eb, ShouldEqual, c.expect2nd)
+				})
 			})
 		}
 	})
