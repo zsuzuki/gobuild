84ae087226a13b7be07d4782847d7bc763deb8e0
Masashi Fujita <objectxtreme@gmail.com>
2017-07-12 15:16:04 +0900

    feat: Define compilation-database related things

---
+++ compiledb.go
@@ -0,0 +1,61 @@
+package main
+
+import (
+	"encoding/json"
+	"github.com/pkg/errors"
+	"io"
+	"io/ioutil"
+	"os"
+	"path/filepath"
+)
+
+// CompileDbItem represents an entry for json compilation database (https://clang.llvm.org/docs/JSONCompilationDatabase.html)
+type CompileDbItem struct {
+	// The working directory
+	Directory string `json:"directory"`
+	// The source file
+	File string `json:"file"`
+	// The output
+	Output string `json:"output"`
+	// Compilation command
+	Arguments []string `json:"arguments"`
+}
+
+// CraeteCompileDbFile creates compilation-database file.
+func CreateCompileDbFile(outPath string, defs []CompileDbItem) error {
+	var err error
+
+	outDir := filepath.Dir(outPath)
+	tmpOut, err := ioutil.TempFile(outDir, "cdb-")
+	if err != nil {
+		return errors.Wrapf(err, "failed to create temporal output for \"%s\"", outPath)
+	}
+	defer (func() {
+		tmpOut.Close()
+		os.Remove(tmpOut.Name())
+	})()
+	err = WriteCompileDb(tmpOut, defs)
+	if err != nil {
+		errors.Wrapf(err, "failed to write definitions")
+	}
+	tmpOut.Close()
+	err = os.Rename(tmpOut.Name(), outPath)
+	if err != nil {
+		return errors.Wrapf(err, "failed to rename \"%s\" to \"%s\"", tmpOut.Name(), outPath)
+	}
+	return nil
+}
+
+// WriteCompileDb writes definitions to output.
+func WriteCompileDb(output io.Writer, defs []CompileDbItem) error {
+	var err error
+	b, err := json.MarshalIndent(defs, "", "    ")
+	if err != nil {
+		return errors.Wrapf(err, "failed to marshal definitions")
+	}
+	cnt, err := output.Write(b)
+	if err != nil || cnt != len(b) {
+		return errors.Wrapf(err, "failed to write marshaled definitions")
+	}
+	return nil
+}
