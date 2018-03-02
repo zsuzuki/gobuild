aee2ec6a8097021180c3904c3201946294afe55c
objectx <objectx@polyphony.co.jp>
2018-03-02 17:04:45 +0900

    fix: Fix a wrong environment variable reference

--- compiler_launcher.go
+++ compiler_launcher.go
@@ -8,7 +8,7 @@ import (
 // FindCompilerLanuncher probes compiler launcher if exists.
 // Currently only probes SN-DBS launcher.
 func FindCompilerLauncher() string {
-	dir, ok := os.LookupEnv("SCE_SDK_ROOT")
+	dir, ok := os.LookupEnv("SCE_ROOT_DIR")
 	if ok {
 		lancherPath := filepath.Join(dir, "Common", "SN-DBS", "bin", "dbsbuild.exe")
 		if _, err := os.Stat(lancherPath); err == nil {
